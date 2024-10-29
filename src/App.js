582a1230cb7bbaa3c58633d98e5975145f85d559
himan1009 <shishusha922@gmail.com>
2024-10-29 07:29:44 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+
+import "./App.css";
+import List from "./Components/List/List";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
+  const statusListArray = [
+    "In progress",
+    "Backlog",
+    "Todo",
+    "Done",
+    "Cancelled",
+  ];
+  const userListArray = [
+    "Anoop sharma",
+    "Yogesh",
+    "Shankar Kumar",
+    "Ramesh",
+    "Suresh",
+  ];
+  const priorityListArray = [
+    { name: "No priority", priority: 0 },
+    { name: "Low", priority: 1 },
+    { name: "Medium", priority: 2 },
+    { name: "High", priority: 3 },
+    { name: "Urgent", priority: 4 },
+  ];
+
+  const [selectedGroupValue, setSelectedGroupValue] = useState(
+    getSavedStateFromLocalStorage() || "status"
+  );
+  const [selectedOrderValue, setSelectedOrderValue] = useState("title");
+  const [ticketDetailsArray, setTicketDetailsArray] = useState([]);
+
+  const orderTicketDataByValue = useCallback(
+    async (ticketsArray) => {
+      if (selectedOrderValue === "priority") {
+        ticketsArray.sort((a, b) => b.priority - a.priority);
+      } else if (selectedOrderValue === "title") {
+        ticketsArray.sort((a, b) => a.title.localeCompare(b.title));
+      }
+      setTicketDetailsArray(ticketsArray);
+    },
+    [selectedOrderValue]
+  );
+
+  function saveStateToLocalStorage(stateToSave) {
+    localStorage.setItem("groupValue", JSON.stringify(stateToSave));
+  }
+
+  function getSavedStateFromLocalStorage() {
+    const savedState = localStorage.getItem("groupValue");
+    return savedState ? JSON.parse(savedState) : null;
+  }
+
+  useEffect(() => {
+    saveStateToLocalStorage(selectedGroupValue);
+
+    const fetchAndProcessData = async () => {
+      const response = await axios.get(
+        "https://api.quicksell.co/v1/internal/frontend-assignment "
+      );
+      if (response.status === 200) {
+        const ticketArray = response.data.tickets.map((ticket) => ({
+          ...ticket,
+          userObj: response.data.users.find(
+            (user) => user.id === ticket.userId
+          ),
+        }));
+        setTicketDetailsArray(ticketArray);
+        orderTicketDataByValue(ticketArray);
+      }
+    };
+
+    fetchAndProcessData();
+  }, [orderTicketDataByValue, selectedGroupValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar
+        groupValue={selectedGroupValue}
+        orderValue={selectedOrderValue}
+        handleGroupValue={setSelectedGroupValue}
+        handleOrderValue={setSelectedOrderValue}
+      />
+      <section className="board-details">
+        <div className="board-details-list">
+          {
+            {
+              status: statusListArray.map((status) => (
+                <List
+                  key={status}
+                  groupValue="status"
+                  orderValue={selectedOrderValue}
+                  listTitle={status}
+                  ticketDetails={ticketDetailsArray}
+                />
+              )),
+              user: userListArray.map((user) => (
+                <List
+                  key={user}
+                  groupValue="user"
+                  orderValue={selectedOrderValue}
+                  listTitle={user}
+                  ticketDetails={ticketDetailsArray}
+                />
+              )),
+              priority: priorityListArray.map((priority) => (
+                <List
+                  key={priority.priority}
+                  groupValue="priority"
+                  orderValue={selectedOrderValue}
+                  listTitle={priority.priority}
+                  ticketDetails={ticketDetailsArray}
+                />
+              )),
+            }[selectedGroupValue]
+          }
+        </div>
+      </section>
+    </>
   );
 }
 
